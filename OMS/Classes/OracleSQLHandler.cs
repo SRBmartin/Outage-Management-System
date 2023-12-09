b606a43b0ed6adef5c88ac14dfdc174be27d70cf
Мартин Мухаремовић <makimuh02@gmail.com>
2023-12-09 21:00:59 +0100

    added initial project and intial oraclesqlclass
    I have created and added an initial project and a class OracleSQL class for handling connection with database.

---
+++ OMS/Classes/OracleSQLHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMS.Classes
+{
+    class OracleSQLHandler
+    {
+        string user;
+        string user_pwd;
+        string db_name;
+        string db_pwd;
+    }
+}
