8afc9c513e67f102483a1f46e121e43ab7439709
Мартин Мухаремовић <makimuh02@gmail.com>
2023-12-14 09:09:37 +0100

    Adding Oracle SQL handler classes
    Added classes which will be used for establishing connection and safely closing the connection.

---
+++ OMS/Classes/DatabaseHandlerClasses/OracleSQLConnectionParams.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace OMS.Classes.DatabaseHandlerClasses
+{
+    class OracleSQLConnectionParams
+    {
+        private static readonly string localDataSource = "//localhost:1521/xe";
+        private static readonly string userId = "oms_projekat";
+        private static readonly string password = "ftn";
+        protected static string getConnectionString()
+        {
+            OracleConnectionStringBuilder ocsb = new OracleConnectionStringBuilder();
+            ocsb.DataSource = localDataSource;
+            ocsb.UserID = userId;
+            ocsb.Password = password;
+            return ocsb.ConnectionString;
+        }
+    }
+}
